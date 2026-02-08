473643881fc5f78036835cbc280545004df56022
jcmacallan-svg <j.c.macallan@gmail.com>
2026-02-08 21:38:26 +0100

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 // Paste your Google Apps Script Web App URL here.
 // If empty, logging is disabled (only local device counter is used).
 window.APP_CONFIG = {
-  logEndpoint: "PASTE_YOUR_APPS_SCRIPT_WEBAPP_URL_HERE"
+  logEndpoint: "https://script.google.com/macros/s/AKfycbx6ijbIHpsW478LEaPVvGiz-eKi8uRzxCGSXp7ABZ8/dev"
   // The endpoint should accept JSON POST:
   // {event, student, className, runId, ts, stats}
 };
