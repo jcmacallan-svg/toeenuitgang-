a532b642f2f541a2bbcbcf7997e13a69ffcc1490
jcmacallan-svg <j.c.macallan@gmail.com>
2026-02-09 06:25:46 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,14 @@
 window.APP_CONFIG = {
-  voiceAutoSend: true,
+  // Voice
+  voiceAutoSend: true,          // autosend when releasing the mic button
+  voiceLang: "en-US",           // recognition language
 
   // Teacher mode (hidden editor)
   showTeacherButton: true,
   teacherHotkey: "ctrl+alt+shift+p",
   teacherPin: "",
 
+  // Logging
   logEndpoint: "https://script.google.com/macros/s/AKfycbxACQ3xg43B-zHm2x3jX0B3Q9Cqf5cs4CSJtejdMKHZ4nKksQ7C2i_pVuapOWaA66gu/exec",
   logUnknownQuestions: true
 };
