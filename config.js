deb803ac7d180a6666bebfd046776fe5bbe6badb
jcmacallan-svg <j.c.macallan@gmail.com>
2026-02-11 11:16:03 +0100

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-// config.js
-// Tweak these without touching app.js
-window.CONFIG = {
-  voiceAutoSend: true,
-  assetBase: "assets/photos",
-  headshotPrefix: "headshot_",
-  headshotCount: 10
-};
